bab41636b6a45b4eabfa5e4edb440b7bba492f2f
Henry Lin <henrylin1207@gmail.com>
2025-04-25 16:08:19 +1000

    set up project

---
+++ src/App.tsx
@@ -0,0 +1,49 @@
+import { useCallback, useEffect } from "react";
+import {
+  ReactFlow,
+  Background,
+  Controls,
+  MiniMap,
+  addEdge,
+  useNodesState,
+  useEdgesState,
+  type OnConnect,
+} from "@xyflow/react";
+import "@xyflow/react/dist/style.css";
+import { initialNodes, nodeTypes } from "./nodes";
+import { initialEdges, edgeTypes } from "./edges";
+
+const getInitialNodes = () => {
+  const stored = localStorage.getItem("cld-nodes");
+  return stored ? JSON.parse(stored) : initialNodes;
+};
+
+export default function App() {
+  const [nodes, , onNodesChange] = useNodesState(getInitialNodes());
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  const onConnect: OnConnect = useCallback(
+    (connection) => setEdges((edges) => addEdge(connection, edges)),
+    [setEdges]
+  );
+
+  useEffect(() => {
+    localStorage.setItem("cld-nodes", JSON.stringify(nodes));
+  }, [nodes]);
+
+  return (
+    <ReactFlow
+      nodes={nodes}
+      nodeTypes={nodeTypes}
+      onNodesChange={onNodesChange}
+      edges={edges}
+      edgeTypes={edgeTypes}
+      onEdgesChange={onEdgesChange}
+      onConnect={onConnect}
+      fitView
+    >
+     <Background />
+      <MiniMap />
+      <Controls />
+    </ReactFlow>
+  );
+}
