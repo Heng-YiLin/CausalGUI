9b1767cf6afaad7fe7410fb17f3546e597f2213c
Henry Lin <henrylin1207@gmail.com>
2025-05-19 00:57:23 +1000

    seperate CLD component

---
+++ src/components/CLD.tsx
@@ -0,0 +1,51 @@
+import { useCallback, useEffect } from "react";
+import {
+  ReactFlow,
+  Background,
+  Controls,
+  MiniMap,
+  addEdge,
+  useNodesState,
+  useEdgesState,
+  type OnConnect,
+} from "@xyflow/react";
+import "@xyflow/react/dist/style.css";
+import { initialNodes, nodeTypes } from "../nodes";
+import { initialEdges, edgeTypes } from "../edges";
+
+const getInitialNodes = () => {
+  const stored = localStorage.getItem("cld-nodes");
+  return stored ? JSON.parse(stored) : initialNodes;
+};
+
+export default function CLD() {
+  const [nodes, , onNodesChange] = useNodesState(getInitialNodes());
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  const onConnect: OnConnect = useCallback(
+    (connection) => setEdges((edges) => addEdge(connection, edges)),
+    [setEdges]
+  );
+
+  useEffect(() => {
+    localStorage.setItem("cld-nodes", JSON.stringify(nodes));
+  }, [nodes]);
+
+  return (
+    <div style={{ height: "90%" }}>
+      <ReactFlow
+        nodes={nodes}
+        nodeTypes={nodeTypes}
+        onNodesChange={onNodesChange}
+        edges={edges}
+        edgeTypes={edgeTypes}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        fitView
+      >
+        <Background />
+        <MiniMap />
+        <Controls />
+      </ReactFlow>
+    </div>
+  );
+}
